2e3abffb585211dc0e9479ff88234aac663d0f2b
BuilderSIA <lordallplay@gmail.com>
2024-08-19 20:26:55 +0500

    about us editing

--- src/components/SinglePage.jsx
+++ src/components/SinglePage.jsx
@@ -1,18 +1,84 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useParams } from "react-router-dom"
+import { UseGlobalContext } from "./Context"
 
 
 const SinglePage = () => {
-    const {id} = useParams()
+    const {data} = UseGlobalContext();
+    const {id} = useParams();
+    let items = [].concat(data.winter,data.spring,data.summer,data.autumn);
+    let currentItem = items.find((item)=>item.id==id)
   return (
     <div className="single-box">
-      <h2>
-        Hello single page
-      </h2>
-      <h1>
-        {id}
-      </h1>
+      <img src={currentItem.image} alt="" />
+      <div className="single-info">
+
+      <h2>{currentItem.name}</h2>
+      <table className="single-info-table">
+        <tbody className="single-info-table-body">
+          <tr className="single-info-table-row">
+            <td className="single-info-table-column">Material:</td>
+            <td className="single-info-table-column" >{currentItem.material}</td>
+          </tr>
+          <tr className="single-info-table-row">
+            <td className="single-info-table-column">Yostiq kiyimi:</td>
+            <td className="single-info-table-column" >{currentItem.yostiqKiyimi}</td>
+          </tr>
+          <tr className="single-info-table-row">
+            <td className="single-info-table-column">Choyshab:</td>
+            <td className="single-info-table-column" >{currentItem.choyshab}</td>
+          </tr>
+          <tr className="single-info-table-row">
+            <td className="single-info-table-column">Ko'rpa-to'shak:</td>
+            <td className="single-info-table-column" >{currentItem.korpaToshak}</td>
+          </tr>
+          <tr className="single-info-table-row">
+            <td className="single-info-table-column">Hajmi:</td>
+            <td className="single-info-table-column" >{currentItem.hajmi}</td>
+          </tr>
+          <tr className="single-info-table-row">
+            <td className="single-info-table-column">Ishlab chiqaruvchi:</td>
+            <td className="single-info-table-column">{currentItem.ishlabChiqargan}</td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
     </div>
   )
 }
 
 export default SinglePage
+{/* <div>
+      <h2>Ikat</h2>
+      <table>
+        <tbody>
+          <tr>
+            <td>Material:</td>
+            <td>100% paxta flanel</td>
+          </tr>
+          <tr>
+            <td>Yostiq kiyimi:</td>
+            <td>50x70 sm (2 dona)</td>
+          </tr>
+          <tr>
+            <td>Choyshab:</td>
+            <td>260×280 sm (1 dona)</td>
+          </tr>
+          <tr>
+            <td>Ko'rpa-to'shak:</td>
+            <td>160x220 sm (2 dona)</td>
+          </tr>
+          <tr>
+            <td>Hajmi:</td>
+            <td>Maxsus o'lcham</td>
+          </tr>
+          <tr>
+            <td>Ishlab chiqaruvchi:</td>
+            <td>Buxoro tabiiy mahsuloti</td>
+          </tr>
+        </tbody>
+      </table>
+      <p>
+        Kategoriya: <span style={{ color: 'red' }}>Qishki kolleksiya</span>
+      </p>
+    </div> */}
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,8 +1,83 @@
+import { UseGlobalContext } from "../components/Context"
 
 const AboutUs = () => {
+    const {t} = UseGlobalContext();
   return (
-    <div>
-      About us
+    <div className="aboutus">
+      <div className="aboutus-info">
+        <h2 className="aboutus-info-head">
+          {t("btm")}
+        </h2>
+        <ul className="aboutus-info-list">
+          <li className="aboutus-info-list-item">
+          <span>&#183;</span>
+              <p>
+              {t("about-1")}
+              </p>
+          </li>
+          <li className="aboutus-info-list-item">
+          <span>&#183;</span>
+              <p>
+              {t("about-2")}
+              </p>
+          </li>
+          <li className="aboutus-info-list-item">
+          <span>&#183;</span>
+              <p>
+              {t("about-3")}
+              </p>
+          </li>
+          <li className="aboutus-info-list-item">
+          <span>&#183;</span>
+              <p>
+              {t("about-4")}
+              </p>
+          </li>
+          <li className="aboutus-info-list-item">
+          <span>&#183;</span>
+              <p>
+              {t("about-5")}
+              </p>
+          </li>
+          <li className="aboutus-info-list-item">
+          <span>&#183;</span>
+              <p>
+              {t("about-6")}
+              </p>
+          </li>
+          <li className="aboutus-info-list-item">
+          <span>&#183;</span>
+              <p>
+              {t("about-7")}
+              </p>
+          </li>
+          <li className="aboutus-info-list-item">
+          <span>&#183;</span>
+              <p>
+              {t("about-8")}
+              </p>
+          </li>
+          <li className="aboutus-info-list-item">
+          <span>&#183;</span>
+              <p>
+              {t("about-9")}
+              </p>
+          </li>
+          <li className="aboutus-info-list-item">
+          <span>&#183;</span>
+              <p>
+              {t("about-10")}
+              </p>
+          </li>
+          <li className="aboutus-info-list-item">
+          <span>&#183;</span>
+              <p>
+              {t("about-11")}
+              </p>
+          </li>
+        </ul>
+      </div>
+      <img src="/aboutus.png" alt="" />
     </div>
   )
 }
